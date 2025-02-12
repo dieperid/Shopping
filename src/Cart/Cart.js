c09c4b90584e5f7332333520ae2053568188c6fc
David Dieperink <david.dieperink@ik.me>
2025-02-12 11:19:36 +0100

    refactor: created `checkEmptyCart()` method to throw the EmptyCart exception, refactored `count()` and `total()`methods to user reduce function of array

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -10,12 +10,19 @@ module.exports = class CartItem {
         this.#items = items;
     }
 
+    checkEmptyCart() {
+        if (this.#items == null || this.#items <= 0) {
+            throw new EmptyCartException("Cart can't be empty");
+        }
+    }
+
     add(items) {
         if (items == null || items.length <= 0) {
             throw new UpdateCartException(
                 "Failed to update cart items, items can't be empty"
             );
         }
+
         if (this.#items) {
             this.#items.push(...items);
         } else {
@@ -24,41 +31,30 @@ module.exports = class CartItem {
     }
 
     count(value) {
-        if (this.#items == null || this.#items.length <= 0) {
-            throw new EmptyCartException("Cart can't be empty");
-        }
-
-        let count = 0;
+        this.checkEmptyCart();
 
         if (value) {
-            count = this.#items.length;
-        } else {
-            for (let item of this.#items) {
-                count += item.quantity;
-            }
+            // Return the number of CartItem
+            return this.#items.length;
         }
 
-        return count;
+        // Return the sum of quantity of CartItem
+        return this.#items.reduce(
+            (totalQuantity, item) => totalQuantity + item.quantity,
+            0
+        );
     }
 
     get total() {
-        let sum = 0;
-
-        if (this.#items == null || this.#items.length <= 0) {
-            throw new EmptyCartException("Cart can't be empty");
-        }
-
-        for (let item of this.#items) {
-            sum += item.quantity * item.price;
-        }
-
-        return sum;
+        this.checkEmptyCart();
+        return this.#items.reduce(
+            (sum, item) => sum + item.quantity * item.price,
+            0
+        );
     }
 
     get items() {
-        if (this.#items == null || this.#items <= 0) {
-            throw new EmptyCartException("Cart can't be empty");
-        }
+        this.checkEmptyCart();
         return this.#items;
     }
 };
